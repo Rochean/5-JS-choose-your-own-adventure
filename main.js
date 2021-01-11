dc3786178dabe0f96e512843998da3978ae22821
Rochean <Angela.Rochester49@gmail.com>
2021-01-11 18:39:25 -0500

    completed diner game

---
+++ main.js
@@ -0,0 +1,19 @@
+var stillAlive = true
+
+alert("Welcome to Angela's")
+var foodChoice = prompt ('What can I get you, a salad, a salad and soda, or a burger')
+    if (foodChoice ==='burger') {
+        stillAlive = false
+        alert ('bad choice, you lose, you are dead.')
+    } else if (foodChoice ==='salad') {
+        alert ('good choice, you win')
+    } else {
+        alert ('make better choices, you still win')
+    if (saladandSoda) {
+        alert ('not too bad, you win')
+    } else if (foodChoice ==='veggie platter') {
+        stillAlive = true
+        alert('excellent choice, you win')
+    }
+    }
+     
